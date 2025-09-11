7bc3183023fe583fa9d0a767dcdea9f09121029a
Kevin Rao <kevin.rao@abingdon.org.uk>
2025-09-11 15:04:35 +0100

    Updated lists-task/lists.py Changelog:  + Stricter type checking  + More elegant implementations with fewer hard-coded values  - Bug: after ValueError is caught, means and sums aren't calculated properly.

--- lists-task/lists.py
+++ lists-task/lists.py
@@ -1,17 +1,22 @@
 name_list = ['Alp', 'Carter', 'Longyu', 'Samuel', 'Teo', 'Ryan', 'Oscar', 'George', 'Isaac', 'Kevin', 'Henry', 'Henry', 'Papa', 'Aidan', 'Thomas']
 
 for i in range(3):
-    name = input("Type in a name: ")
+    name = input(f"Type in name {i + 1} of 3: ")
     name_list.append(name)
 print(name_list)
-print(f"The third name is: {name_list[1]}")
-print("The last 7 names are: ")
-for i in range(len(name_list)-8, len(name_list)):
-    print(f"{name_list[i]} ")
+print(f"The third name is: {name_list[2]}")
+print(f"The last 7 names are: {name_list[-7:]}")
 
 number_list = []
-for i in range(5):
-    number_list.append(int(input("Type in a number: ")))
+while True:
+    try:
+        number_count = int(input("Number of numbers to add: "))
+        for i in range(number_count):
+            number_list.append(float(input(f"Type in number {i + 1} of {number_count}: ")))
+        break
+    except ValueError:
+        print("Number of numbers to add must be integer")
 print(f"largest: {max(number_list)}")
 print(f"smallest: {min(number_list)}")
-print(f"mean: {sum(number_list)/5}")
+print(f"sum: {sum(number_list)}")
+print(f"mean: {sum(number_list)/len(number_list)}")
