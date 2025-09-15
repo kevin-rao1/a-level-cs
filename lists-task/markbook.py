84fcff7626919f557ef40d244c651a92e30fc9b8
Kevin Rao <kevin.rao@abingdon.org.uk>
2025-09-15 20:31:42 +0100

    Changed markbook.py Changelog:  + proper error handling  - removed all calls to renderer.py

--- lists-task/markbook.py
+++ lists-task/markbook.py
@@ -1,16 +1,11 @@
-try: 
-    import keyboard
-except ModuleNotFoundError:
-    print("Please install the 'keyboard' library.")
-    exit()
-from renderer import test
 name_list =  ['Ryansh', 'Oliver', 'Edward', 'Daniel', 'Alex', 'Ares', 'Thomas', 'Andy', 'Charlie', 'Ryan', 'Lucas']
 scores_list = [[] for i in name_list]           # Create a lists of empty lists
-render_state = "home"
-test(render_state, name_list, scores_list)
 while True:
-    name = input("Enter name: ")                # Get the user to enter a name
-    name_index = name_list.index(name)          # Find out which index that name appears at
-    new_score = int(input("Enter score: "))     # The user enters a new score for that person
-    scores_list[name_index].append(new_score)   # Append the new score to the list in the relevant location within scores
-    print(f"New average score: {sum(scores_list[name_index])/len(scores_list[name_index])}")
+    try:
+        name = input("Enter name: ")                # Get the user to enter a name
+        name_index = name_list.index(name)          # Find out which index that name appears at
+        new_score = int(input("Enter score: "))     # The user enters a new score for that person
+        scores_list[name_index].append(new_score)   # Append the new score to the list in the relevant location within scores
+        print(f"New average score: {sum(scores_list[name_index])/len(scores_list[name_index])}")
+    except ValueError:
+        print("Name is not in list. Check spelling and capitalisation.")
