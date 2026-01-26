5719ef83f7ba9764bb8d12d49d784f82f2a045e4
Kevin Rao <kevin.rao@abingdon.org.uk>
2026-01-26 21:14:59 +0000

    finished decimal to binary conversion

---
+++ conversion-algorithm/dec-to-bin.py
@@ -0,0 +1,11 @@
+def convert_decimal_to_binary(decimal:int):
+    binary = []
+    if decimal == 0:
+        return 0
+    while not decimal == 0:
+        dec_new = decimal//2
+        binary.insert(0,decimal%2)
+        decimal = dec_new
+    return binary
+
+print(convert_decimal_to_binary(9008))
